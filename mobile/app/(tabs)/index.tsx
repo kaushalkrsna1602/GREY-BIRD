71489821e643548d44da01a08077f609362d5f72
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-07-12 02:46:46 +0530

    user added to DB

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useUserSync } from '@/hooks/useUserSync';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
+  useUserSync();
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-white">
       <Text>HomeScreen</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
